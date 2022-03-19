998539f023f1840101304d4c7c679e4c80c07070
franc <franc@LAPTOP-BLJRH6NF>
2022-03-19 17:32:03 -0300

    Meu primeiro commit no projeto. Foi criado o arquivo .gitignore e retiramos os comentarios da aplicação.

--- src/pacote/principal/calculadora/Principal.java
+++ src/pacote/principal/calculadora/Principal.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package pacote.principal.calculadora;
 
-/**
- *
- * @author franc
- */
 public class Principal {
     public static void main(String[] args) {
         
